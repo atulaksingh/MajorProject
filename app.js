b2e6d6fba46bb004aed8a9c4902c0b0784f73538
atulaksingh <talktoatulsingh@gmail.com>
2025-07-25 18:10:07 +0530

    update all routes

--- app.js
+++ app.js
@@ -1,19 +1,80 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+const Listing = require("./models/listing");
+const path = require("path");
+const methodOverride = require("method-override");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 const mongoURI = "mongodb://127.0.0.1:27017/wanderlust";
+app.use(methodOverride("_method"));
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(" MongoDB connected successfully"))
+  .catch((err) => console.error(" MongoDB connection error:", err));
 
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log(" MongoDB connected successfully"))
-.catch((err) => console.error(" MongoDB connection error:", err));
+app.get("/", async (req, res) => {
+  let allListings = await Listing.find({});
+  res.render("listings/index", { allListings });
+});
 
-app.get("/",(req,res)=>{
-    res.send("hello lesting")
-})
+// New Routes
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//Create Route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/");
+});
+
+
+app.get("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+res.render("listings/show", { listing });
+});
+
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/");
+});
+
+// app.get("/listings/:id", async (req, res) => {
+//   let { id } = req.params;
+//   const listing = await Listing.findById(id);
+// res.render("listing/show", { listing });
+
+// });
+
+// app.get("/listing", async (req, res) => {
+//   const allListings = await Listing.find({});
+//   res.render("listing/index", { allListings });
+// });
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
+
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing");
+// });
 
 app.listen(8080, () => {
   console.log("Server is running on port 8000");
